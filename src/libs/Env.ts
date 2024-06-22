b250c74d7345a266960f81e6e62596ede3d75dd7
JtotheDoubleO <joo291998@gmail.com>
2024-06-23 04:26:13 +0700

    fix(Env): modified env validation & fixed ip package

--- src/libs/Env.ts
+++ src/libs/Env.ts
@@ -5,8 +5,6 @@ import { z } from 'zod';
 export const Env = createEnv({
   server: {
     CLERK_SECRET_KEY: z.string().min(1),
-    DATABASE_URL: z.string().min(1),
-    DATABASE_AUTH_TOKEN: z.string().optional(),
     LOGTAIL_SOURCE_TOKEN: z.string().optional(),
   },
   client: {
@@ -17,8 +15,6 @@ export const Env = createEnv({
   // You need to destructure all the keys manually
   runtimeEnv: {
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    DATABASE_URL: process.env.DATABASE_URL,
-    DATABASE_AUTH_TOKEN: process.env.DATABASE_AUTH_TOKEN,
     LOGTAIL_SOURCE_TOKEN: process.env.LOGTAIL_SOURCE_TOKEN,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
