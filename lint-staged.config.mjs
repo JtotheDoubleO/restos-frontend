4a0230e5195a1ad2edd79c1db63f762633d247c8
JtotheDoubleO <joo291998@gmail.com>
2024-06-28 18:59:09 +0700

    fix(Lint): fixed lint stage

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
+export default {
+  '*.{ts,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
   '**/*.ts?(x)': () => 'npm run check-types',
   '*.{json,yaml}': ['prettier --write'],
 };
